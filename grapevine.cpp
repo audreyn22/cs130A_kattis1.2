c5a9ca8abf4f6dea13be5ef0fa3d1b8ce0bc77a5
Audrey Nguyen <a_nguyen2@csilvm-12.cs.ucsb.edu>
2025-10-23 11:51:39 -0700

    fixed debugging errors

--- grapevine.cpp
+++ grapevine.cpp
@@ -6,12 +6,14 @@
 
 using namespace std;
 
-
+//input of graph, starting person (vertex), number of days rumor spreads, and level of skeptism
 void BFS(unordered_map<string, vector<string>> &adj, string start, int numDays, unordered_map<string, int> &track){
     unordered_map<string, int> shareCount;
     unordered_map<string, bool> knowsRumor;
     queue<pair<string, int>> temp;
 
+    //person 0 knows rumor
+    //pushes onto queue
     knowsRumor[start] = true;
     temp.push(make_pair(start, 0));
     
@@ -26,10 +28,12 @@ void BFS(unordered_map<string, vector<string>> &adj, string start, int numDays, 
             for(int i = 0; i < adj[currPerson].size(); i++){
                 string nextTo = adj[currPerson][i];
                 shareCount[nextTo]++;
-
-                if(!knowsRumor[nextTo] && shareCount[nextTo] >= track[nextTo]){
+                knowsRumor[nextTo] = true;
+                if(shareCount[nextTo] > 0){
                     knowsRumor[nextTo] = true;
-                    temp.push(make_pair(nextTo, currDay + 1));
+                    if(shareCount[nextTo] == track[nextTo]){
+                        temp.push(make_pair(nextTo, currDay + 1));
+                    }
                 }
             }
         }
@@ -48,18 +52,20 @@ void BFS(unordered_map<string, vector<string>> &adj, string start, int numDays, 
 int main()
 {
     std::ios::sync_with_stdio(false); std::cin.tie(NULL);
+    //stores input number of people, numbers of connections, and how many days
     int people, connections, numDays;
     cin >> people >> connections >> numDays;
 
+    //stores each person's level of skeptism
     unordered_map<string, int> track;
     string name;
     int level;
-
     for(int i = 0; i < people; i++){
         cin >> name >> level;
         track[name] = level;
     }
 
+    //creates undirected adjacency list (graph) of connections between people
     unordered_map<string, vector<string>> adj;
     string person1, person2;
     for(int j = 0; j < connections; j++){
@@ -68,9 +74,11 @@ int main()
         adj[person2].push_back(person1);
     }
 
+    //stores who starts with rumor
     string start;
     cin >> start;
     
+    //run BFS
     BFS(adj, start, numDays, track);
 
     return 0;
